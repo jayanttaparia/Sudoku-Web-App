22171dc107e76d4da8c8a53bafbe4b5c9375000d
Jayant Taparia <jayanttaparia969@gmail.com>
2020-05-25 23:58:15 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,135 @@
+var arr = [[], [], [], [], [], [], [], [], []]
+var temp = [[], [], [], [], [], [], [], [], []]
+
+for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) {
+        arr[i][j] = document.getElementById(i * 9 + j);
+    }
+}
+
+function initializeTemp(temp) {
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            temp[i][j] = false;
+        }
+    }
+}
+
+
+function setTemp(board, temp) {
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            if (board[i][j] != 0) {
+                temp[i][j] = true;
+            }
+        }
+    }
+}
+
+
+function setColor(temp) {
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            if (temp[i][j] == true) {
+                arr[i][j].style.color = "#DC3545";
+            }
+        }
+    }
+}
+
+function resetColor() {
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            arr[i][j].style.color = "green";
+        }
+    }
+}
+
+var board = [[], [], [], [], [], [], [], [], []]
+
+
+let button = document.getElementById('generate-sudoku')
+let solve = document.getElementById('solve')
+
+console.log(arr)
+function changeBoard(board) {
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            if (board[i][j] != 0) {
+                arr[i][j].innerText = board[i][j]
+            }else
+                arr[i][j].innerText = ''
+        }
+    }
+}
+
+
+button.onclick = function () {
+    var xhrRequest = new XMLHttpRequest()
+    xhrRequest.onload = function () {
+        var response = JSON.parse(xhrRequest.response)
+        console.log("response",response)
+        initializeTemp(temp)
+        resetColor()
+
+        board = response.board
+        setTemp(board, temp)
+        setColor(temp)
+        changeBoard(board)
+    }
+    xhrRequest.open('get', 'https://sugoku.herokuapp.com/board?difficulty=easy')
+    //we can change the difficulty of the puzzle the allowed values of difficulty are easy, medium, hard and random
+    xhrRequest.send()
+}
+
+function isPossible(board, sr, sc, val) {
+    for (var x = 0; x < 9; x++) {
+        if (board[sr][x] == val  || board[x][sc] == val) {
+            return false;
+        }
+    }
+
+    var r = sr - sr % 3;
+    var c = sc - sc % 3;
+
+    for (var cr = r; cr < r + 3; cr++) {
+        for (var cc = c; cc < c + 3; cc++) {
+            if (board[cr][cc] == val) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function solveSudokuHelper(board, sr, sc) {
+    if (sr == 9) {
+        changeBoard(board);
+        return true;
+    }
+    if (sc == 9) {
+        return solveSudokuHelper(board, sr + 1, 0)
+    }
+
+    if (board[sr][sc] != 0) {
+        return solveSudokuHelper(board, sr, sc + 1);
+    }
+
+    for (var i = 1; i <= 9; i++) {
+        if (isPossible(board, sr, sc, i)) {
+            board[sr][sc] = i;
+            if(solveSudokuHelper(board, sr, sc + 1))
+                return true;
+            board[sr][sc] = 0;
+        }
+    }
+    return false;
+}
+
+function solveSudoku(board) {
+    solveSudokuHelper(board, 0, 0)
+}
+
+solve.onclick = function () {
+    solveSudoku(board)
+}
